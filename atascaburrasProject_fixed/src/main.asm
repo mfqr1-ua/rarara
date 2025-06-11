5c155a774b5f569ef4bc9f8c64528d5ccc677404
mfqr1-ua <98946818+mfqr1-ua@users.noreply.github.com>
2025-06-11 23:34:33 +0200

    Add game system stub and ignore build outputs

--- atascaburrasProject_fixed/src/main.asm
+++ atascaburrasProject_fixed/src/main.asm
@@ -2,13 +2,16 @@
 INCLUDE "src/utils/constants.asm"
 INCLUDE "src/utils/memory.asm"
 INCLUDE "src/utils/render.asm"
+INCLUDE "src/system/game_system.asm"
 INCLUDE "src/ui/tiles.asm"
 INCLUDE "src/ui/maps.asm"
 
 SECTION "Main", ROM0
 Start:
     call InitRender
+    call InitGameSystem
 
 .loop:
+    call UpdateGameSystem
     call RenderFrame
     jp .loop
---
+++ atascaburrasProject_fixed/src/system/game_system.asm
@@ -0,0 +1,9 @@
+SECTION "GameSystem", ROM0
+
+InitGameSystem::
+    ; initialize game-related variables
+    ret
+
+UpdateGameSystem::
+    ; update game state each frame
+    ret
