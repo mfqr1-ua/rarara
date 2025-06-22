e27d30873acc7faefc5f7474ee4aaaad06851836
mfqr1-ua <mfqr1@alu.ua.es>
2025-06-22 18:24:23 +0200

    fallo en render.asm

--- atascaburrasProject_fixed/src/utils/render.asm
+++ atascaburrasProject_fixed/src/utils/render.asm
@@ -6,43 +6,49 @@ INCLUDE "src/utils/constants.asm"
 EXPORT Tiles8p8
 EXPORT TilesEnd
 
+; Routines de render
 EXPORT InitRender
 EXPORT RenderFrame
 EXPORT DrawMap
 EXPORT DisplayWinMessage
 
-
-WaitVBlankStart:
+; -----------------------------------------------------------------------------
+; Espera a VBlank
+WaitVBlankStart::
     ld a, [$FF44]
     cp 144
     jr nz, WaitVBlankStart
     ret
 
-SwitchOffScreen:
+; Apaga LCD
+SwitchOffScreen::
     ld a, [$FF40]
     res 7, a
     ld [$FF40], a
     ret
 
-SwitchOnScreen:
+; Enciende LCD
+SwitchOnScreen::
     ld a, [$FF40]
     set 7, a
     ld [$FF40], a
     ret
 
-; Initializes tile data and screen settings
+; -----------------------------------------------------------------------------
+; InitRender: carga tiles y dibuja mapa inicial completo
 InitRender::
     call SwitchOffScreen
 
+    ; Activar tile data a $8000
     ld a, [rLCDC]
-    set 4, a                      ; use $8000 tile data
+    set 4, a
     ld [rLCDC], a
 
-    ; Copy tiles into VRAM
+    ; Copiar banco de tiles Tiles8p8→TilesEnd a VRAM
     ld hl, Tiles8p8
     ld de, $8000
     ld bc, TilesEnd - Tiles8p8
-CopyTiles:
+CopyTiles::
     ld a, [hl+]
     ld [de], a
     inc de
@@ -51,18 +57,92 @@ CopyTiles:
     or b
     jr nz, CopyTiles
 
-    ; Draw initial map in the background
-    ld hl, $9800
+    ; Dibujar fondo (tilemap) completo
+    call DrawMap
+
+    call SwitchOnScreen
+    ret
+
+; -----------------------------------------------------------------------------
+; RenderFrame: actualiza sprite del jugador cada frame
+RenderFrame::
+    call WaitVBlankStart
+
+    ; Restaurar tile de fondo en posición anterior
+    ld a, [PlayerPrevY]
+    ld l, a
+    ld h, 0
+    ; DE = y*4
+    sla l : rl h
+    sla l : rl h
+    ld d, h
+    ld e, l
+    ; HL = y*16 (shift 4 veces)
+    ld l, a
+    ld h, 0
+    sla l : rl h
+    sla l : rl h
+    sla l : rl h
+    sla l : rl h
+    add hl, de            ; HL = y*20
+    ; añadir x
+    ld a, [PlayerPrevX]
+    ld e, a
+    ld d, 0
+    add hl, de            ; offset
+    ; sumar base de mapa
     ld a, [CurrentMapPtr]
     ld e, a
     ld a, [CurrentMapPtr+1]
     ld d, a
-    ld b, MAP_HEIGHT
-RowLoop:
-    ld c, MAP_WIDTH
-ColLoop:
+    add hl, de
+    ; B = tile original
+    ld b, [hl]
+    ; escribirlo
+    ld a, b
+    ld [hl], a
+
+    ; Dibujar jugador en posición actual
+    ld a, [PlayerY]
+    ld l, a
+    ld h, 0
+    ; HL = y*32 (shift 5 veces)
+    sla l : rl h
+    sla l : rl h
+    sla l : rl h
+    sla l : rl h
+    sla l : rl h
+    ; añadir x
+    ld a, [PlayerX]
+    ld e, a
+    ld d, 0
+    add hl, de
+    ; sumar base tilemap
+    ld de, $9800
+    add hl, de
+    ; colocar tile jugador
+    ld a, MT_PLAYER
+    ld [hl], a
+    ret
+
+; -----------------------------------------------------------------------------
+; DrawMap: dibuja tilemap completo usando CurrentMapPtr
+DrawMap::
+    call WaitVBlankStart
+    ld hl, $9800
+    ; cargar puntero
+    ld de, CurrentMapPtr
     ld a, [de]
+    ld e, a
     inc de
+    ld a, [de]
+    ld d, a
+    ld b, MAP_HEIGHT
+RowLoop::
+    ld c, MAP_WIDTH
+ColLoop::
+    ld a, [e]
+    inc e
     ld [hl+], a
     dec c
     jr nz, ColLoop
@@ -74,148 +154,41 @@ ColLoop:
     ld h, a
     dec b
     jr nz, RowLoop
-
-    call SwitchOnScreen
     ret
 
-; Renders the current frame (map and player)
-RenderFrame::
-    call WaitVBlankStart
-    ; Placeholder for additional rendering
-    ; Restore tile at previous player position
-    ld a, [PlayerPrevY]
-    ld l, a
-    ld h, 0
-    add hl, hl
-    add hl, hl
-    ld d, h
-    ld e, l                  ; DE = y*4
-    ld a, [PlayerPrevY]
-    ld l, a
-    ld h, 0
-    add hl, hl
-    add hl, hl
-    add hl, hl
-    add hl, hl               ; HL = y*16
-    add hl, de               ; HL = y*20
-    ld a, [PlayerPrevX]
-    ld e, a
-    ld d, 0
-    add hl, de               ; HL = tile offset
-    ld a, [CurrentMapPtr]
-    ld e, a
-    ld a, [CurrentMapPtr+1]
-    ld d, a
-    add hl, de
-    ld b, [hl]               ; B = original tile
+; -----------------------------------------------------------------------------
+; WinMessage: cadena y longitud usando EQU
+WinMessage:
+    db TILE_Y, TILE_O, TILE_U, TILE_SPACE, TILE_W, TILE_I, TILE_N
+WinMessageLen EQU . - WinMessage
 
-    ; Compute VRAM address for previous position
-    ld a, [PlayerPrevY]
-    ld l, a
-    ld h, 0
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h                    ; HL = 32 * prevY
-    ld a, [PlayerPrevX]
-    ld e, a
-    ld d, 0
-    add hl, de
-    ld de, $9800
-    add hl, de
-    ld a, b
-    ld [hl], a              ; restore background tile
-
-    ; Draw player at current position
-    ld a, [PlayerY]
-    ld l, a
-    ld h, 0
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h
-    sla l
-    rl h                    ; HL = 32 * Y
-    ld a, [PlayerX]
-    ld e, a
-    ld d, 0
-    add hl, de
-    ld de, $9800
-    add hl, de
-    ld a, MT_PLAYER
-    ld [hl], a              ; draw player tile
-    ret
-
-; Draws the current map pointed by CurrentMapPtr to the background
-DrawMap::
-    call WaitVBlankStart
-    ld hl, $9800
-    ld a, [CurrentMapPtr]
-    ld e, a
-    ld a, [CurrentMapPtr+1]
-    ld d, a
-    ld b, MAP_HEIGHT
-.RowLoop:
-    ld c, MAP_WIDTH
-.ColLoop:
-    ld a, [de]
-    inc de
-    ld [hl+], a
-    dec c
-    jr nz, .ColLoop
-    ld a, l
-    add a, $20 - MAP_WIDTH
-    ld l, a
-    ld a, h
-    adc a, 0
-    ld h, a
-    dec b
-    jr nz, .RowLoop
-    ret
-
-; Displays "YOU WIN" centered on the screen
+; -----------------------------------------------------------------------------
+; DisplayWinMessage: limpia fondo y escribe YOU WIN centrado
 DisplayWinMessage::
-    ; Wait for VBlank to safely update VRAM
     call WaitVBlankStart
-    ; Hide the screen while preparing the win screen
     call SwitchOffScreen
 
-    ; Clear the entire background to blank tiles
+    ; Limpiar todo el tilemap a TILE_SPACE
     ld hl, $9800
-    ld bc, $0400            ; 32x32 background size
-    xor a                   ; tile 0 is blank/white
-.clear_loop:
+    ld bc, $0400        ; 32×32
+    ld a, TILE_SPACE
+ClearLoop::
     ld [hl+], a
     dec bc
     ld a, c
     or b
-    jr nz, .clear_loop
+    jr nz, ClearLoop
 
-    ; Draw the win message centred on screen
+    ; Escribir mensaje en row 9,col 6 (p.ej. $9926)
     ld hl, WinMessage
-    ld de, $9926            ; row 9, column 6
+    ld de, $9926
     ld b, WinMessageLen
-.char_loop:
+WriteChar::
     ld a, [hl+]
     ld [de], a
     inc de
     dec b
-    jr nz, .char_loop
+    jr nz, WriteChar
 
-    ; Show the screen with the message
     call SwitchOnScreen
     ret
-
-WinMessage:
-    db TILE_Y, TILE_O, TILE_U, MT_FLOOR, TILE_W, TILE_I, TILE_N
-DEF WinMessageLen = @-WinMessage
